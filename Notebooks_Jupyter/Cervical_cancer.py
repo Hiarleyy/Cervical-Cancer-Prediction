61cd68c5fd4d73ce78f85ca2fd5b5aca0f304a33
Marcos Hiarley <marcoshiarley.silva@gmail.com>
2024-10-11 09:40:54 -0300

    Remove unnecessary commit files and add data processing and visualization to Cervical cancer analysis notebook

--- Notebooks_Jupyter/Cervical_cancer.py
+++ Notebooks_Jupyter/Cervical_cancer.py
@@ -22,4 +22,23 @@ sb.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
 plt.show()
 
 
+
+# %%
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('kag_risk_factors_cervical_cancer.csv',sep  = ",",na_values = '?')
+colunas_para_remover = ['STDs: Time since first diagnosis', 'STDs: Time since last diagnosis']  # Substitua pelos nomes das colunas que deseja remover
+df = df.drop(columns=colunas_para_remover)
+
+# %%
+df['Dx:Cancer'].value_counts(normalize=True)
+df['Dx:Cancer'].value_counts().plot(kind="bar", color=["salmon", "lightblue"]);
+
+# %%
+pd.crosstab(df["Dx:Cancer"], df["Smokes"])
+# %%
+pd.crosstab(df["Dx:Cancer"], df["Smokes"]).plot(kind="bar",
+                                            figsize=(10,6),
+                                            color=["green", "blue"]);
 # %%
